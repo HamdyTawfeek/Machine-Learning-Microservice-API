e49e8b3f84f3d64db3301bc7199acabce36cef8b
Hamdy <hamdy.t@trufla.com>
2020-09-21 04:24:11 +0200

    Created Dockerfile and scripts to run it and push to dockerhub

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build --tag=mlapi .
 # Step 2: 
 # List docker images
-
+docker images
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 mlapi
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="hamdymostafa/mlapi"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag mlapi $dockerpath:1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:1.0
