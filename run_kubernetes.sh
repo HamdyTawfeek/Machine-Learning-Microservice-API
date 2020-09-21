813706b3116c5250e7fe4d5ac7a4435a146ffe0d
Hamdy <hamdy.t@trufla.com>
2020-09-21 05:57:01 +0200

    Running the app from a kubernetes cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="hamdymostafa/mlapi:1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mlapiservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapiservice 8000:80 
