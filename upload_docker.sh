8295e264b55d92dbe1b6e06f21404a14361327f7
Hamdy <hamdy.t@trufla.com>
2020-09-21 05:55:58 +0200

    update docker path

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,14 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath="hamdymostafa/mlapi"
+dockerpath="hamdymostafa/mlapi:1.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login
-docker tag mlapi $dockerpath:1.0
+docker tag mlapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath:1.0
+docker push $dockerpath
